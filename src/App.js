01f7f30186bc33a8f3c2ce42c3bc31bb23670e1c
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-04 10:33:24 +0200

    Update Routing structure

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './components/home';
-import store from './redux/configureStore';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import './index.css';
@@ -7,9 +8,11 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
