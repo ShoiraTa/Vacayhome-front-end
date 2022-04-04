1d5b72f84510cd3362149039cf108f4a94d289cf
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-04 16:01:55 +0200

    Add Register component Route

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </>
   );
