a3001b91fd94ed0e439a94af0cad9f5a8f365847
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-04 14:49:53 +0200

    Add Login page routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
     <>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
