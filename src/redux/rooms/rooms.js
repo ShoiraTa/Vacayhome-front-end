17ef3aabc69b6a96cdc5ce502189dd6beee4805e
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-05 22:17:01 +0100

    Update rooms.js file

--- src/redux/rooms/rooms.js
+++ src/redux/rooms/rooms.js
@@ -1,13 +1,25 @@
+import Axios from 'axios';
+import houseUrl from './house_url';
+
+const FETCH_DATA = 'houses/FETCH_DATA';
+// const UPDATE_STATE = 'rooms/UPDATE_STATE';
+
+export const fetchHouses = () => async (dispatch) => {
+  const response = await Axios.get(houseUrl);
+  dispatch({
+    type: FETCH_DATA,
+    payload: response.data,
+  });
+};
+
 const initialState = [];
-const FETCH_DATA = 'rooms/FETCH_DATA';
-const UPDATE_STATE = 'rooms/UPDATE_STATE';
 
 const roomsReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_DATA:
       return action.payload;
-    case UPDATE_STATE:
-      return action.payload;
+    // case UPDATE_STATE:
+    //   return action.payload;
     default:
       return state;
   }
