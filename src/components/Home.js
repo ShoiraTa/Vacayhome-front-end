3c3dbf2484e12cae9d940ae65ab5140cb2cdc19e
shoiraTa <shoira.shakirovna@gmail.com>
2022-04-05 16:06:51 +0400

    Add carousel on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,22 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
+import SliderHomepage from './SliderHomepage';
 
 function Home() {
   return (
     <>
-      <h1 className="mb-5">Welcome to Vacay Home</h1>
+      <div className="bg-white homepage-container">
+        <div className="homepage-header-container">
 
-      <Carousel fade className="mt-5">
-        <Carousel.Item>
-          <img className="d-block home-images" src="https://www.lovethispic.com/uploaded_images/106830-Tropical-Vacation-Bedroom-With-Panoramic-Ocean-View.jpg" alt="Tropical Vacation Bedroom" />
-          <Carousel.Caption>
-            <p>Wake up and feel the Ocean breeze</p>
-            <button type="submit">See more details</button>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block home-images" src="https://media.istockphoto.com/photos/3d-rendering-beautiful-luxury-bedroom-suite-in-hotel-with-tv-and-picture-id1213885014?k=20&m=1213885014&s=612x612&w=0&h=8TV57yZ2jqldDOjR8sK7xADHLH_0cSMXY-KXNLoS1Jw=" alt="Tropical Vacation Bedroom" />
-          <Carousel.Caption>
-            <p>Enjoy comfort at it&apos;s best</p>
-            <button type="submit">See more details</button>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block home-images" src="https://www.simplemost.com/wp-content/uploads/2016/06/17388775_772072786294452_4370047079117432062_o.jpg" alt="Tropical Vacation Bedroom" />
-          <Carousel.Caption>
-            <p>Sleep under the Northern lights with this comfortable igloo home</p>
-            <button type="submit">See more details</button>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block home-images" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/37/df/96/sandals-south-coast.jpg?w=900&h=-1&s=1" alt="Tropical Vacation Bedroom" />
-          <Carousel.Caption>
-            <p>Luxury suite in the carribean</p>
-            <button type="submit">See more details</button>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+          <h1>Top-rated vacation rentals</h1>
+          <p>Find and book unique accommodations</p>
+        </div>
+        <div>
+
+          <SliderHomepage />
+        </div>
+
+      </div>
+
     </>
   );
 }
---
+++ src/components/SliderHomepage.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+function Slider() {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 454 },
+      items: 2,
+      slidesToSlide: 2, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+  return (
+    <Carousel
+      arrows
+      renderButtonGroupOutside
+      swipeable
+      draggable={false}
+      responsive={responsive}
+      infinite
+      autoPlaySpeed={8000}
+      keyBoardControl
+      customTransition="all .5"
+      transitionDuration={500}
+      containerClass="carousel-container"
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      <div className="carousel-img-container">
+        <a href="/login">
+          <img
+            src="https://images.squarespace-cdn.com/content/v1/5e72c8bfe21ad940ba788673/1621016255763-WDHM79J11KCDQW4DIKFX/airbnb-short-term-versus-long-term-rentals-thumbnail.jpg"
+            className="d-block w-100"
+            alt="travel vacation"
+          />
+        </a>
+
+        <div className="carousel-description">
+          <span>3&nbsp;  guests1&nbsp;  bedroom2&nbsp;  beds1&nbsp;  bath</span>
+          <p>
+            <strong>Marisha’s Apartment&nbsp; </strong>
+            Light and cosy apartment with good location
+          </p>
+        </div>
+      </div>
+      <div className="carousel-img-container">
+        {' '}
+        <a href="/login">
+          <img
+            src="https://media.glampinghub.com/CACHE/images/accommodations/accommodationsaccommodation_20067_listing_image_3312cf45f2bd4132b66383de82807b23-glamping-f7618228-f263-4c69-b8f8-5c73254173e2/b9284237867e3bcf30a77b248c7835ea.jpg"
+            className="d-block w-100"
+            alt="travel vacation"
+          />
+        </a>
+        <div className="carousel-description">
+          <span>3&nbsp;  guests1&nbsp;  bedroom2&nbsp;  beds1&nbsp;  bath</span>
+          <p>
+            <strong>Marisha’s Apartment&nbsp; </strong>
+            Light and cosy apartment with good location
+          </p>
+        </div>
+      </div>
+      <div className="carousel-img-container">
+        {' '}
+        <a href="/login">
+          <img
+            src="https://www.extraspace.com/blog/wp-content/uploads/2020/10/preparing-home-for-airbnb.jpg"
+            className="d-block w-100"
+            alt="travel vacation"
+          />
+        </a>
+        <div className="carousel-description">
+          <span>3&nbsp;  guests1&nbsp;  bedroom2&nbsp;  beds1&nbsp;  bath</span>
+          <p>
+            <strong>Marisha’s Apartment&nbsp; </strong>
+            Light and cosy apartment with good location
+          </p>
+        </div>
+      </div>
+      <div className="carousel-img-container">
+        <a href="/login">
+          <img
+            src="https://rdcnewscdn.realtor.com/wp-content/uploads/2019/04/Airbnb-Rental-Property-1.jpg"
+            className="d-block w-100"
+            alt="travel vacation"
+          />
+        </a>
+        <div className="carousel-description">
+          <span>3&nbsp;  guests1&nbsp;  bedroom2&nbsp;  beds1&nbsp;  bath</span>
+          <p>
+            <strong>Marisha’s Apartment&nbsp; </strong>
+            Light and cosy apartment with good location
+          </p>
+        </div>
+      </div>
+    </Carousel>
+  );
+}
+
+export default Slider;
