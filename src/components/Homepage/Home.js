1bf61c2f1c5d5c7481f5643e5ef8b39bc995bd8e
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-06 18:42:56 +0200

    Update Navbar layout and file structure

--- src/components/Homepage/Home.js
+++ src/components/Homepage/Home.js
@@ -3,31 +3,31 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import SliderHomepage from './SliderHomepage';
-import Brand from '../Brand';
-import SideBar from '../SideBar';
+import Brand from '../Navbar/Brand';
+import SideBar from '../Navbar/SideBar';
+import Social from '../Navbar/Social';
 
 function Home() {
   return (
     <>
-      <div className="bg-white homepage-container">
-        <Brand />
-        <div className="homepage-header-container">
-          <h1>Top-rated vacation rentals</h1>
-          <p>Find and book unique accommodations</p>
-        </div>
-        <Container fluid>
-          <Row>
-            <Col sm={2} md={2} lg={2}>
-              <SideBar />
-            </Col>
-            <Col sm={10} md={10} lg={10}>
-              <div>
-                <SliderHomepage />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container fluid>
+        <Row className="homepage-container bg-white">
+          <Col sm={2} md={2} lg={2} className="d-flex flex-column justify-content-between bg-light px-0">
+            <Brand />
+            <SideBar />
+            <Social />
+          </Col>
+          <Col sm={10} md={10} lg={10}>
+            <div className="homepage-header-container">
+              <h1>Top-rated vacation rentals</h1>
+              <p>Find and book unique accommodations</p>
+            </div>
+            <div>
+              <SliderHomepage />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- src/components/Navbar/SideBar.js
+++ src/components/Navbar/SideBar.js
@@ -3,17 +3,17 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 function SideBar() {
   return (
-    <ListGroup defaultActiveKey="/">
-      <ListGroup.Item action href="/" variant="warning">
+    <ListGroup defaultActiveKey="/" variant="flush">
+      <ListGroup.Item action href="/" variant="success">
         LISTINGS
       </ListGroup.Item>
-      <ListGroup.Item action href="#link2" variant="warning">
+      <ListGroup.Item action href="#link2" variant="success">
         PLACES
       </ListGroup.Item>
-      <ListGroup.Item action href="#link3" variant="warning">
+      <ListGroup.Item action href="#link3" variant="success">
         BOOKINGS
       </ListGroup.Item>
-      <ListGroup.Item action href="#link4" variant="warning">
+      <ListGroup.Item action href="#link4" variant="success">
         ADD LISTING
       </ListGroup.Item>
     </ListGroup>
