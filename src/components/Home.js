6adaeb1c69ae229a1e13287436aaa8528245da9a
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-06 12:12:32 +0200

    Create and add Brand navbar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import SliderHomepage from './SliderHomepage';
+import Brand from './Brand';
 
 function Home() {
   return (
     <>
       <div className="bg-white homepage-container">
+        <Brand />
         <div className="homepage-header-container">
-
           <h1>Top-rated vacation rentals</h1>
           <p>Find and book unique accommodations</p>
         </div>
         <div>
-
           <SliderHomepage />
         </div>
-
       </div>
-
     </>
   );
 }
