be5cd30b12c5ac9c45aa8c832717671e21dd93f4
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-07 15:08:39 +0100

    Fix code bug and make details page responsive

--- src/components/Homepage/SliderHomepage.js
+++ src/components/Homepage/SliderHomepage.js
@@ -18,7 +18,7 @@ function Slider() {
       slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 375, min: 0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
