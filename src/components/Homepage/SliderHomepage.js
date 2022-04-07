aea0f18436963d4b341234e7fab8d0665fe9c1ba
shoiraTa <shoira.shakirovna@gmail.com>
2022-04-07 21:16:50 +0400

    Add responsiveness

--- src/components/Homepage/SliderHomepage.js
+++ src/components/Homepage/SliderHomepage.js
@@ -11,15 +11,15 @@ function Slider() {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
       items: 3,
-      slidesToSlide: 3, // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     tablet: {
-      breakpoint: { max: 1024, min: 454 },
+      breakpoint: { max: 1024, min: 767 },
       items: 2,
-      slidesToSlide: 2, // optional, default to 1.
+      slidesToSlide: 1, // optional, default to 1.
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 765, min: 0 },
       items: 1,
       slidesToSlide: 1, // optional, default to 1.
     },
