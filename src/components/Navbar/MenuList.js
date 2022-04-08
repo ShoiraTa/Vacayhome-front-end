347bab68399e8a6ab69fc110d7f63da36cec37bc
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-04-08 18:53:36 +0200

    Update MenuList active functionality and styling

--- src/components/Navbar/MenuList.js
+++ src/components/Navbar/MenuList.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import ListGroup from 'react-bootstrap/ListGroup';
 
 function MenuList() {
   return (
-    <ListGroup defaultActiveKey="/" variant="flush" className="d-none d-md-block">
-      <ListGroup.Item action href="/" variant="success">
+    <ListGroup variant="flush" className="d-none d-md-block">
+      <ListGroup.Item action as={NavLink} to="/" className="nav-item">
         LISTINGS
       </ListGroup.Item>
-      <ListGroup.Item action href="/1/0/reservation" variant="success">
+      <ListGroup.Item action as={NavLink} to="/1/0/reservation" className="nav-item">
         ADD RESERVATION
       </ListGroup.Item>
-      <ListGroup.Item action href="/1/reservations" variant="success">
+      <ListGroup.Item action as={NavLink} to="/1/reservations" className="nav-item">
         MY RESERVATIONS
       </ListGroup.Item>
-      <ListGroup.Item action href="/new_listing" variant="success">
+      <ListGroup.Item action as={NavLink} to="/new_listing" className="nav-item">
         ADD LISTING
       </ListGroup.Item>
-      <ListGroup.Item action href="#destroy" variant="success">
+      <ListGroup.Item action as={NavLink} to="/delete_listing" className="nav-item">
         DELETE LISTING
       </ListGroup.Item>
     </ListGroup>
