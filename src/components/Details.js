147dffbea6ef96f35e6460fb9fbdc21a1033d6e8
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-10 20:46:46 +0100

    Complete styling for details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BiArrowBack } from 'react-icons/bi';
 import '../details.css';
 import Card from 'react-bootstrap/Card';
@@ -6,60 +6,77 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { fetchRooms } from '../redux/rooms/rooms';
-import Brand from './Navbar/Brand';
-import SideBar from './Navbar/SideBar';
-import Social from './Navbar/Social';
-
 
 const userid = localStorage.getItem('userId');
 const Detail = (props) => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRooms());
+  }, []);
+
+  const rooms = useSelector((state) => state.roomsReducer);
+
+  const { houseid } = useParams();
+
+  let house = {};
+
+  rooms.map((element) => {
+    if (element.id === parseInt(houseid, 10)) {
+      house = element;
+    }
+    return house;
+  });
+
   const {
     image_url: imageUrl, name, description, price, address, city, country,
   } = props.props;
   return (
     <>
-      <div className="details-container">
-        <div className="flex-direction">
-          <div>
-            <img className="images" src={imageUrl} alt="Tropical houses" />
-          </div>
-          <Card.Body>
-            <div className="details-text-container">
+      <Container fluid>
+        <Row className="homepage-container bg-white">
+          <div className="details-container">
+            <div className="flex-direction">
               <div>
-                <h3>{name}</h3>
-                <p className="description my-4">{description}</p>
-                <div>
-                  <p className="description">
-                    This vacation home is located in
-                    {' '}
-                    {address}
-    &nbsp;
-                    {city}
-    &nbsp;
-                    {country}
-                  </p>
-                  <p className="description">
-                    For the price of
-                    {' '}
-                    $
-                    {price}
-                  </p>
-                </div>
-                <div>
-                  <button variant="success" className="btn-success reserve-btn mt-4" size="lg" type="button">Reserve</button>
-                </div>
-                <div>
-                  <a href={`/${userid}`}>
-                    <BiArrowBack className="mt-4" />
-                  </a>
-                </div>
+                <img className="images" src={imageUrl} alt="Tropical houses" />
               </div>
+              <Card.Body>
+                <div className="details-text-container">
+                  <div>
+                    <h3>{name}</h3>
+                    <p className="description my-4">{description}</p>
+                    <div>
+                      <p className="description">
+                        This vacation home is located in
+                        {' '}
+                        {address}
+    &nbsp;
+                        {city}
+    &nbsp;
+                        {country}
+                      </p>
+                      <p className="description">
+                        For the price of
+                        {' '}
+                        $
+                        {price}
+                      </p>
+                    </div>
+                    <div>
+                      <button variant="success" className="btn-success reserve-btn mt-4" size="lg" type="button">Reserve</button>
+                    </div>
+                    <div>
+                      <a href={`/${userid}`}>
+                        <BiArrowBack className="mt-4" />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </Card.Body>
             </div>
-          </Card.Body>
-        </div>
-      </div>
+          </div>
+        </Row>
+      </Container>
     </>
   );
 };
