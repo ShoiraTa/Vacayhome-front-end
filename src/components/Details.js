2e1df1f94d818735992ddf3d607424e374b5dca7
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-10 20:21:46 +0100

    Import card and adjust details.js file

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BiArrowBack } from 'react-icons/bi';
 import '../details.css';
+import Card from 'react-bootstrap';
 
 const userid = localStorage.getItem('userId');
 const Detail = (props) => {
@@ -9,38 +10,42 @@ const Detail = (props) => {
   } = props.props;
   return (
     <>
-      <div className="flex">
-        <div>
-          <img className="images" src={imageUrl} alt="Tropical houses" />
-        </div>
-        <div>
-          <h3>{name}</h3>
-          <p>{description}</p>
-        </div>
-        <div>
-          <p>
-            This vacation home is located in
-            {' '}
-            {address}
-&nbsp;
-            {city}
-&nbsp;
-            {country}
-          </p>
-          <p>
-            For the price of
-            {' '}
-            $
-            {price}
-          </p>
-        </div>
-        <div>
-          <button type="button">Reserve</button>
-        </div>
-        <div>
-          <a href={`/${userid}`}>
-            <BiArrowBack />
-          </a>
+      <div className="details-container">
+        <div className="flex-direction">
+          <div>
+            <img className="images" src={imageUrl} alt="Tropical houses" />
+          </div>
+          <Card.Body>
+            <div>
+              <h3>{name}</h3>
+              <p>{description}</p>
+            </div>
+            <div>
+              <p>
+                This vacation home is located in
+                {' '}
+                {address}
+  &nbsp;
+                {city}
+  &nbsp;
+                {country}
+              </p>
+              <p>
+                For the price of
+                {' '}
+                $
+                {price}
+              </p>
+            </div>
+            <div>
+              <button type="button">Reserve</button>
+            </div>
+            <div>
+              <a href={`/${userid}`}>
+                <BiArrowBack />
+              </a>
+            </div>
+          </Card.Body>
         </div>
       </div>
     </>
