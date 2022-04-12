f9709996cc7e2bdf7a030761315d9c1da697d40e
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-12 12:48:17 +0100

    Write test for home page

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -20,7 +20,7 @@ function Homepage({ isLoggedIn }) {
   return (
     <div>
       <div className="homepage-header-container">
-        <h1>Top-rated vacation rentals</h1>
+        <h1 id="h1">Top-rated vacation rentals</h1>
         <p>Find and book unique accommodations</p>
       </div>
       <Slider />
--- src/components/_tests_/components/Homepage.test.js
+++ src/components/_tests_/components/Homepage.test.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import '@testing-library/react';
-// import { render } from '@testing-library/react';
-import renderer from 'react-test-renderer';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter as Router } from 'react-router-dom';
-// import store from '../../../redux/configureStore';
+// import '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+// import { renderer, screen } from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from '../../../redux/configureStore';
 import Homepage from '../../Homepage/Homepage';
 
-it('matches Homepage snapshot', () => {
-  const homepage = renderer.create(
-    <Homepage />,
-  ).toJSON();
-  expect(homepage).toMatchSnapshot();
+// it('matches Homepage', () => {
+//   const homepage = renderer.create(
+//     <Homepage />,
+//   ).toJSON();
+//   expect(homepage).toHaveBeenCalled();
+// });
+
+describe('Home page', () => {
+  test('it renders the given name in the greeting', () => {
+    render(<Provider store={store}><Router><Homepage /></Router></Provider>);
+    expect(screen.getByText('Top-rated vacation rentals')).toBeInTheDocument();
+  });
 });
