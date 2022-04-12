f8bed87ec46c8af1fab189f1ae8209b13c69d1e5
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-12 11:05:53 +0100

    Write test for home page

--- src/components/_tests_/components/Homepage.test.js
+++ src/components/_tests_/components/Homepage.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
-import store from '../../../redux/configureStore';
+import '@testing-library/react';
+// import { render } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import store from '../../../redux/configureStore';
 import Homepage from '../../Homepage/Homepage';
+
+it('matches Homepage snapshot', () => {
+  const homepage = renderer.create(
+    <Homepage />,
+  ).toJSON();
+  expect(homepage).toMatchSnapshot();
+});
