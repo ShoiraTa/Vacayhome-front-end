c5e9b03f74651e667a694f8464c3671c5afde66d
Ugboaja Uchechi <74814780+Ugboaja-Uchechi@users.noreply.github.com>
2022-04-12 14:33:10 +0100

    Remove comments

--- src/components/_tests_/components/Homepage.test.js
+++ src/components/_tests_/components/Homepage.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import '@testing-library/react';
 import { render, screen } from '@testing-library/react';
-// import { renderer, screen } from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../../../redux/configureStore';
